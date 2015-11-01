def61da234151e9b398b84497ef544bc430f99da
Frantisek Farkas <frantisek.farkas93@gmail.com>
2015-11-01 23:32:26 +0100

    Added palindrome func

--- proj1.c
+++ proj1.c
@@ -3,13 +3,24 @@
  */
 #include<stdio.h>
 #include <time.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
+
+    int num = 4;
+    if (is_prime(num) == 1) {
+        printf("je prime\n");
+    }
+    else {
+        printf("nieje prime\n");
+    }
+    char sent[100] = "wow";
+
+    is_palindrome(sent);
 
     return 0;
 
 }
-
 
 int is_number() {
 
@@ -22,38 +33,38 @@ int is_number() {
 
 }
 
-
 int is_prime(int number_to_test) {
     int i;
-
-    for (i = 3; i < number_to_test; i++) {
-        if (((i ^ (number_to_test - 1)) % number_to_test) == 1);
-        else {
+    for (i = 2; i < number_to_test; i++) {
+        if (number_to_test % i == 0 && i != number_to_test)
             return 0;
-        }
+
     }
     return 1;
 
 }
 
-int is_palindrome(char *sentence) {
+int is_palindrome(char sentence[100]) {
 
-    int length;
-    int j;
-    int mid;
 
-    length = sizeof(sentence);
-    mid = length / 2;
+    int begin, middle, end, length = 0;
 
-    for (j = 0; j < mid; j++) {
-        char left = sentence[j];
-        char right = sentence[length - j - 1];
 
-        if (left != right) {
-            return 0;
+    while (sentence[length] != '\0')
+        length++;
+
+    end = length - 1;
+    middle = length / 2;
+
+    for (begin = 0; begin < middle; begin++) {
+        if (sentence[begin] != sentence[end]) {
+            printf("Not a palindrome.\n");
+            break;
         }
+        end--;
     }
-    return 1;
+    if (begin == middle)
+        printf("Palindrome.\n");
 
 }
 
